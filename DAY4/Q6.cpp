16c51718c3fce33323fdfd98b751163f60bf9b49
RohanBarwade <rohanbarwade05189@gmail.com>
2024-03-29 09:50:20 +0530

    1st changes

---
+++ DAY4/Q6.cpp
@@ -0,0 +1,18 @@
+/*
+2.	Print all Odd numbers from 1 to n, 
+take n as an input from the user.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the last number\n";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+    
+    if(i%2==1)
+    cout<<i<<endl;
+
+    }
+
+}
